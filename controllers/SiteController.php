e320ec80e5d5de773ba484f505968634d83de5c8
LosBandittos <zou1990@yandex.ru>
2025-11-24 11:21:45 +0300

    minor fix

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -8,6 +8,13 @@ use yii\filters\AccessControl;
 use yii\web\Controller;
 
 class SiteController extends Controller {
+    public function actions() {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
 
     public function behaviors() {
         return [
